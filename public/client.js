e76da7f855450084735abf24ea802aefce1c973a
Christopher Ronning <christopher.a.ronning@gmail.com>
2016-04-30 19:42:11 -0400

    separate logins into module

--- public/client.js
+++ public/client.js
@@ -3,7 +3,8 @@
 const http = require('http');
 const request = require('request');
 
-const config = "../config.js";
+const logins = require('./loginClient.js');
+const config = require("../config.js");
 const host = "http://localhost:8000";
 
 const stdin = process.stdin, stdout = process.stdout;
@@ -38,116 +39,13 @@ function handleInput(input, handlers, onFail) {
 
 // -------------------------
 
-// opening request -- wrapped in function to allow recursion
-// in case of invalid input
-
-function begin() {
-  request(host, function (error, response, body) {
-    acceptInput(body, function (input) {
-      handleInput(input, {
-        'new': newUser,
-        'login': login,
-        'quit': exit
-      }, begin);
-    });
-  });
-}
 
 
-function newUser() {
-  
-  acceptInput("Select a username", function(username) {
-    acceptInput("Select a password", function(password) {
-      let options = {
-        uri: host + '/users',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: {
-          'username': username,
-          'password': password
-        },
-        json: true
-      };
 
-      request(options, function (error, response, body) {
-        if (error) {
-          console.log(Error("Entry invalid") + response.statusCode);
-          exit(1);
-          
-        } else if (response.statusCode == 401) {
-          
-          acceptInput(body, function(input) {
-            handleInput(input, {
-              'new': newUser,
-              'login': login,
-              'quit': exit
-            }, newUser);
-          });
-        } else if (response.statusCode != 200) {
-          console.log(Error(error + response.statusCode));
-          exit(1);
-          
-        } else {
-          console.log(body);
-          // enter chat from here
-          exit();
-        }
-      });
-    });
-  });
-  
-}
-
-function login() {
-
-  // ask for username and password
-  acceptInput("Enter your username", function(username) {
-    acceptInput("Enter your password", function(password) {
-      let options = {
-        uri: host + '/users/authenticate',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: {
-          'username': username,
-          'password': password
-        },
-        json: true
-      };
-      
-      request(options, function(error, response, body) {
-
-        if (error) { console.log(Error("Invalid request" + error.statusCode)); }
-      
-        if (response.body.success == false) {
-          console.log(body.message);
-          acceptInput("Maybe consider signing up with a new username.\n" +
-                      "To do that, enter `new`.\n" +
-                      "To continue trying to log in, type `login`", function(input) {
-            handleInput(input, {
-              'new': newUser,
-              'login': login,
-              'quit': exit
-              }, begin);
-          });
-        } else {
-          loggedIn = true;
-          console.log(body.message);
-          // will move to chat from here
-          exit(0);
-        }
-      });
-    });
-  });
-}
 
 function exit(code) {
   console.log("Goodbye!");
   process.exit(code);
 }
 
-// begin with request to host
-begin();
+logins.begin();
